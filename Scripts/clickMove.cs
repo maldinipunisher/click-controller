555d262195ecab00290330c88bb658aa2eaa533a
maldinipunisher <maldinipunisher@gmail.com>
2020-03-29 11:51:06 +0700

    v1

---
+++ Scripts/clickMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI; 
+
+[RequireComponent(typeof(Camera))] 
+public class clickMove : MonoBehaviour
+{
+    Camera mainCam;
+    public Transform player;
+    public Transform spawnPlayer;
+    Rigidbody rbPlayer;
+    NavMeshAgent agent;
+    public ParticleSystem clickParticle;
+
+    void Start()
+    {
+        mainCam = GetComponent<Camera>();
+        agent = player.GetComponent<NavMeshAgent>();
+        rbPlayer = player.GetComponent<Rigidbody>();
+        player.position = spawnPlayer.position;
+        clickParticle.enableEmission = false; 
+    }
+
+    void Update()
+    {
+        Ray clickPos =  mainCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit; 
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(clickPos,out hit))
+            {
+                agent.SetDestination(hit.point);
+                clickParticle.transform.position = hit.point;
+                clickParticle.enableEmission = true;
+            }
+        }
+    }
+}
---
+++ Scripts/particleSet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleSet : MonoBehaviour
+{
+    ParticleSystem clickParticle;
+
+    private void Start()
+    {
+        clickParticle = GetComponent<ParticleSystem>(); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            clickParticle.enableEmission = false;
+        }
+    }
+}
